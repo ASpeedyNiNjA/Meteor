e039503812a2024c6ead49c2faf7eecfb91e341e
ASpeedyNiNjA <christrombly001@hotmail.com>
2025-05-19 15:01:42 -0400

    Practice 001

--- Player.cs
+++ Player.cs
@@ -2,6 +2,12 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+   // public float speed = 1.0f;
+    private float practiceVariableGoesHere;
+    private bool practiceVariableGoesDown;
+    
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,11 +17,20 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Input Manager Variables
+        practiceVariableGoesHere = Input.GetAxis("Horizontal");
+        practiceVariableGoesDown = Input.GetKey(KeyCode.Space);
+
+
+
         //Attempting #1
         //transform.position = new Vector3(1, 2, 2);
 
         //Attempting #2
-        transform.Translate(0, 1, 0);
+        //transform.Translate(0, 1, 0);
+
+        //Attempting #3
+        transform.Translate(practiceVariableGoesHere * Time.deltaTime * Vector3.right);
 
 
 
